8c9db410c50baa9dcb7542deb9dfa580d72978d8
Michael Breedlove <me@michaelbreedlove.net>
2016-11-22 21:16:51 -0600

    Add original hash for admin menu tab override

--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
+                     :original => 'b7b242ee058673679949d7e3050be5974938294d',
                      :name => "blog_admin_tabs",
                      :insert_bottom => "ul[data-hook='admin_tabs']",
                      :text => "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><%= tab Spree.t(:BLOG, scope: [:admin, :tab]), label: :blog_entries, url: spree.admin_blog_entries_path, icon: 'comment' %><% end %>",
